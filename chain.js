c41e229f110807c7dfd2f9f3875fd3318f357549
SP7-creator <76457031+SP7-creator@users.noreply.github.com>
2021-01-28 18:30:54 -0500

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,20 @@
+class chain{
+constructor(bodyA,bodyB){
+    var options = {
+        bodyA : bird.body,
+        bodyB : log6.body,
+        stiffness : 0.04,
+        length : 5,
+        }
+        this.chain = Constraint.create(options);
+        World.add(world,this.chain);
+}
+display(){
+var pointA = this.chain.bodyA.position;
+var pointB = this.chain.bodyB.position;
+stroke("green");
+strokeWeight(3);
+line(pointA.x,pointA.y,pointB.x,pointB.y)
+}
+
+}
